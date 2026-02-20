fc8ff286306287876b7bfa27b7023c76813419bb
areya0911 <areyavio3@gmail.com>
2026-02-20 09:33:42 +0530

    Login Page-Set

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,13 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import API from "../api/axios";
-import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 export default function Login() {
+  const [role, setRole] = useState("admin");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogin = async () => {
@@ -16,13 +15,13 @@ export default function Login() {
       const res = await API.post("/auth/login", { email, password });
 
       localStorage.setItem("token", res.data.token);
-      login(res.data);
 
       if (res.data.role === "admin") {
         navigate("/admin");
       } else {
         navigate("/user/dashboard");
       }
+
     } catch (err) {
       alert("Invalid credentials");
     }
@@ -31,25 +30,51 @@ export default function Login() {
   return (
     <div className="login-wrapper">
       <div className="login-card">
-        <h2 className="title">EduTrack</h2>
-        <p className="subtitle">Sign in to continue</p>
 
+        <h1 className="logo">EDUTRACK</h1>
+        <p className="subtitle">Engagement Tracking System</p>
+
+        {/* ROLE TOGGLE */}
+        <div className="role-toggle">
+          <button
+            className={role === "admin" ? "active-role" : ""}
+            onClick={() => setRole("admin")}
+          >
+            Admin
+          </button>
+
+          <button
+            className={role === "student" ? "active-role" : ""}
+            onClick={() => setRole("student")}
+          >
+            Student
+          </button>
+        </div>
+
+        {/* EMAIL */}
+        <label>Email Address</label>
         <input
-          className="input"
-          placeholder="Email"
-          onChange={e => setEmail(e.target.value)}
+          type="email"
+          placeholder={role === "admin" ? "admin@edu.in" : "kavya@edu.in"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
+        {/* PASSWORD */}
+        <label>Password</label>
         <input
-          className="input"
           type="password"
-          placeholder="Password"
-          onChange={e => setPassword(e.target.value)}
+          placeholder="••••••••"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button className="login-btn" onClick={handleLogin}>
-          Login
+          Sign In to EduTrack
         </button>
+
+  
+
       </div>
     </div>
   );
